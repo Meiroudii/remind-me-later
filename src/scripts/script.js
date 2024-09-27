290927d9f0caf6214b47e16e2584fffc1af64fab
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2024-09-27 20:22:15 +0900

    add: basic UI

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,12 @@
-console.log("test...");
+const write_entry = document.getElementById("writeEntry"),
+	reminders_container = document.getElementById("reminder_container"),
+	btn_save_entry = document.getElementById("saveEntry");
+
+const new_entry = document.createElement("p");
+
+function save_this_entry() {
+}
+
+btn_save_entry.addEventListener("click", () => {
+	console.log("This entry is working good!");
+});
